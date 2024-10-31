f67fcd8a181185a561ced88902a1c12a6f0e1d27
White <gabrielraimundo@id.uff.br>
2024-10-30 23:33:00 -0300

    initial commit

---
+++ Methods.cs
@@ -0,0 +1,61 @@
+﻿namespace Algs;
+
+public static class Methods
+{
+    public static int Factorial(int num)
+    {
+        if (num == 0)
+        {
+            return 1;
+        }
+        
+        int temp = 1;
+        for (int i = 1; i <= num; i++)
+        {
+            temp *= i;
+        }
+        return temp;
+    }
+
+    public static int Combination(int elements, int positions)
+    {
+        if (positions < 0)
+        {
+            Console.WriteLine("Erro, o número de posições não pode ser menor que zero");
+            return 0;
+        }
+
+        if (positions > elements)
+        {
+            Console.WriteLine("Erro, o número de posições não pode ser maior que o número de elementos");
+        }
+
+        if (positions == 0 || elements == 0)
+        {
+            return 1;
+        }
+
+        if (positions == elements)
+        {
+            return 1;
+        }
+
+        int combination = Factorial(elements) / (Factorial(positions) * Factorial(elements - positions));
+        return combination;
+    }
+    
+    public static void Ppyramid(int rows)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                int tempCombination = Combination(i,j);
+                Console.Write($"{tempCombination} ");
+            }
+            Console.Write("\n");
+        }
+    }
+    
+    
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Algs;
+
+Console.WriteLine("Hello Pascal!");
+
+
+/*int fatA = Methods.Factorial(2);
+int fatB = Methods.Factorial(3);
+int fatC = Methods.Factorial(4);
+
+Console.WriteLine($"O fatorial de 2 é {fatA} \nO de 3 é {fatB} \nO fatorial de 4 é {fatC}");
+Console.WriteLine(Methods.Combination(5,2));*/
+
+Methods.Ppyramid(12);
+
