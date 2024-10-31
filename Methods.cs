c405c673dd7a7aca2aa74178880d833acf68da67
White <gabrielraimundo@id.uff.br>
2024-10-30 23:49:48 -0300

    comments and io system

--- Methods.cs
+++ Methods.cs
@@ -2,6 +2,12 @@
 
 public static class Methods
 {
+    /* Uma função que implementa o funcionamento de Fatorial
+     *
+     * A função recebe o número e calcula o fatorial dele
+     * E claro, conta que o Fatorial de 0 (0!) é igual a 1
+     */
+    
     public static int Factorial(int num)
     {
         if (num == 0)
@@ -17,9 +23,19 @@ public static class Methods
         return temp;
     }
 
+    /* Uma função que implementa a Combinação
+     *
+     * Usa a clássica formula:
+     *
+     *  C(a,b) = a!/b!(a - b)!
+     *
+     *  não aceita números negativos em elementos e nem em posições
+     * 
+     */
+    
     public static int Combination(int elements, int positions)
     {
-        if (positions < 0)
+        if (positions < 0 || elements < 0)
         {
             Console.WriteLine("Erro, o número de posições não pode ser menor que zero");
             return 0;
@@ -43,11 +59,20 @@ public static class Methods
         int combination = Factorial(elements) / (Factorial(positions) * Factorial(elements - positions));
         return combination;
     }
-    
+    /*
+     * Gera a piramide de Pascal
+     */
     public static void Ppyramid(int rows)
-    {
+    { 
+        /* Tendo em vista a formula
+         *
+         * C(a,b) = a!/b!(a-b)!
+         *
+         */
+        // Cada i representa uma linha da pirâmide (Ou um elemento da combinação [O "a" da fórmula])
         for (int i = 0; i < rows; i++)
         {
+            //Cada j representa uma coluna da pirâmide (ou o número de posições da combinação[O "b" da fórmula])
             for (int j = 0; j <= i; j++)
             {
                 int tempCombination = Combination(i,j);
--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
 ﻿using Algs;
 
 Console.WriteLine("Hello Pascal!");
+Console.WriteLine("-");    
+Console.WriteLine("-");    
+Console.WriteLine("Insira o número de linhas que você deseja para a pirâmide: ");
+
+int numbLines = int.Parse(Console.ReadLine());
+Console.WriteLine("- - - - - - - - - - - -");    
+Methods.Ppyramid((numbLines));
 
 
-/*int fatA = Methods.Factorial(2);
-int fatB = Methods.Factorial(3);
-int fatC = Methods.Factorial(4);
-
-Console.WriteLine($"O fatorial de 2 é {fatA} \nO de 3 é {fatB} \nO fatorial de 4 é {fatC}");
-Console.WriteLine(Methods.Combination(5,2));*/
-
-Methods.Ppyramid(12);
 
